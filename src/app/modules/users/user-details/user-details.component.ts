a19a925800351d617c1236fff29d49a191c62892
PiusAsh <pius1ash@gmail.com>
2024-05-13 06:03:48 +0100

    Completed

--- src/app/modules/users/user-details/user-details.component.ts
+++ src/app/modules/users/user-details/user-details.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.scss']
+  styleUrls: ['./user-details.component.scss'],
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit {
+  user: any;
 
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.getUserDetails(id);
+  }
+
+  getUserDetails(id: any): void {
+    this.userService.getUserDetails(id).subscribe(
+      (user) => {
+        this.user = user.data;
+      },
+      (error) => {
+      }
+    );
+  }
 }
